0744ecc5a2d381a2fe88246ccedcb965bed69662
ayesha <ayeshamasood159@hotmail.com>
2021-12-08 02:27:33 +0500

    add new task

--- class05v3/src/index.ts
+++ class05v3/src/index.ts
@@ -4,7 +4,7 @@ import * as inquirer from 'inquirer';
 let todos: TodoItem[] = [
  new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
  new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
-let collection: TodoCollection = new TodoCollection("Adam", todos);
+ let collection: TodoCollection = new TodoCollection("Adam", todos);
 let showCompleted = true;
 function displayTodoList(): void {
  console.log(`${collection.userName}'s Todo List `
@@ -12,8 +12,18 @@ function displayTodoList(): void {
  collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
 }
 enum Commands {
+ Add = "Add New Task",
  Toggle = "Show/Hide Completed",
  Quit = "Quit"
+}
+function promptAdd(): void {
+ console.clear();
+ inquirer.prompt({ type: "input", name: "add", message: "Enter task:"})
+ .then(answers => {if (answers["add"] !== "") {
+ collection.addTodo(answers["add"]);
+ }
+ promptUser();
+ })
 }
 function promptUser(): void {
  console.clear();
@@ -23,14 +33,16 @@ function promptUser(): void {
  name: "command",
  message: "Choose option",
  choices: Object.values(Commands),
- //badProperty: true
  }).then(answers => {
  switch (answers["command"]) {
  case Commands.Toggle:
  showCompleted = !showCompleted;
  promptUser();
  break;
-}
-})
+ case Commands.Add:
+ promptAdd();
+ break;
+ }
+ })
 }
 promptUser();
