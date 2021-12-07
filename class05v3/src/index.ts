0a9137d621795f08a47d49dbf0b2a77f96a11055
ayesha <ayeshamasood159@hotmail.com>
2021-12-08 01:43:42 +0500

    Package save-dev

--- class05v3/src/index.ts
+++ class05v3/src/index.ts
@@ -1,32 +1,15 @@
-import { TodoItem } from "./todoItem";
-import { TodoCollection } from "./todoCollection";
-import * as inquirer from 'inquirer';
-let todos: TodoItem[] = [
-    new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
-    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
-let collection: TodoCollection = new TodoCollection("Adam", todos);
-function displayTodoList(): void {
-    console.log(`${collection.userName}'s Todo List `
-        + `(${collection.getItemCounts().incomplete} items to do)`);
-    collection.getTodoItems(true).forEach(item => item.printDetails());
-}
-enum Commands {
-    Quit = "Quit"
-}
 function promptUser(): void {
     console.clear();
-    displayTodoList();
     inquirer.prompt({
         type: "list",
         name: "command",
         message: "Choose option",
-        choices: Object.values(Commands)
+        choices: Object.values(Commands),
+        badProperty: true
     }).then(answers => {
+        // no action required
         if (answers["command"] !== Commands.Quit) {
             promptUser();
         }
     })
-}
-promptUser();
-if ('answers'["command"] !== Commands.Quit) {
 }
